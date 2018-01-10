21a081114fb01652901ef786b73cba5d99e034db
hmak5215 <hmak5215@lps53.org>
2018-01-10 13:24:17 -0600

    More Additions
    Added JS and stuff

---
+++ Scripts/imageHover.js
@@ -0,0 +1,9 @@
+// JavaScript Document
+
+function hover(element) {
+	element.setAttribute('src', 'Images/TENlogo hover.png');
+}
+
+function unhover(element) {
+	element.setAttribute('src', 'Images/TENlogo.png');
+}
